6a673b3c69af95c6f5e17bc33c210da46bd2cae9
Yuji Nakamura <ny5117@icloud.com>
2023-05-20 10:14:47 +0900

    コミット

---
+++ docs/reference_article.md
@@ -0,0 +1,1 @@
+https://zenn.dev/sum0/articles/9463d16d9d40e2
---
+++ src/Elements/Button/Button.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./";
+
+type T = typeof Button;
+
+export default {
+  component: Button,
+  args: {
+    children: "送信",
+  },
+} satisfies Meta<T>;
+
+export const Default: StoryObj<T> = {};
